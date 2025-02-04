5c3bbb1b39af134ddbfaa295cc4eadcb867073a0
Vinayak Mulugund <mulgundvinay@gmail.com>
2025-02-04 13:24:50 +0530

    Added signin route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -19,6 +19,30 @@ userRouter.post('/signup', validateUserCreationData, async (req: Request, res: R
     } catch (error) {
         res.status(500).json({error, message: "Failed to create user with username " + req.body.username})
     }
-})
+});
+
+userRouter.post('/login', validateUserCreationData, async (req: Request, res: Response) => {
+    try {
+        const user = await UserModel.findOne({username: req.body.username}) as any;
+        if (!user) {
+            res.status(403).json({message: "Wrong Username or password"});
+            return;
+        }
+        try {
+            const match = await user.comparePassword(req.body.password);
+            console.log(match)
+            if (!match) {
+                res.status(403).json({message: "Wrong Username or password"});
+                return;
+            }
+            res.status(201).json({message: "Login Successfull"});
+        } catch (error) {
+            res.status(500).json({message: "Internal server error"})
+        }
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"})
+    }
+});
+
 
 export {userRouter}
--- backend/src/schema/userModel.ts
+++ backend/src/schema/userModel.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from "express";
-import { model, Schema } from "mongoose";
+import { CallbackError, model, Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
 
 const userSchema = new Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) {
+        return;
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error as Error);
+    }
+})
+
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw error
+    }
+}
 
 export const UserModel = model("User", userSchema);
